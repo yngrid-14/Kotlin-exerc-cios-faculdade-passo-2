a2a1cf79bce29ffcab9c13373927917cb020425b
yngrid-14 <trindadelgado@gmail.com>
2025-03-09 14:40:25 -0300

    Add files via upload

---
+++ 4 Par_impar.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Insira um número: ")
+    val num = readln().toInt()
+    if (num%2 == 0){
+    println("O número é par")
+    }
+    else{
+        print("O número é ímpar")
+    }
+}
